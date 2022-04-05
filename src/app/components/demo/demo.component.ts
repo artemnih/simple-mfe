72ea174731b3e418e68ae6158eb1ca28a6ede155
artemnih <23387542+artemnih@users.noreply.github.com>
2022-04-05 16:18:36 -0400

    more stuff

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '@labshare/base-ui-services';
 import { EventKey, EventService } from 'ngx-event-service';
 
 @Component({
@@ -9,19 +10,31 @@ import { EventKey, EventService } from 'ngx-event-service';
 })
 export class DemoComponent implements OnInit {
 
-
+  username = '';
   group = new FormGroup({
     text: new FormControl(''),
   })
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.userManager.getUser().then(user => {
+      if (user) {
+        this.username = user.profile.name as string;
+      }
+    })
   }
 
   submit() {
     const key = new EventKey<string>('demo');
     this.eventService.get(key).next(this.group.value.text);
+  }
+
+  testAuth() {
+    console.log('Client app', this.authService);
+  }
+
+  setV() {
   }
 
 }
